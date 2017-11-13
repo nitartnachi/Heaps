b4cee5446eb1287d77f2d9964e53630906b20a30
Nitin Sharma <nsharma78@gmail.com>
2017-11-12 18:19:25 -0800

    11/12/2017

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Heaps
+
+1) Merge k sorted arrays [Google]
+2) Merge k Sorted Lists *
+3) Find Median from Data Stream
+4) Meeting Rooms II, Meeting Rooms
+5) Range Addition
---
+++ src/main/java/com/nitin/algo/MergeKSortedArrays.java
@@ -0,0 +1,13 @@
+/*
+ * This problem can be solved by using a heap. The time is O(nlog(n)).
+ * Given m arrays, the minimum elements of all arrays can form a heap. It takes O(log(m)) to insert an element to the heap and it takes O(1) to delete the minimum element.
+ */
+package com.nitin.algo;
+
+public class MergeKSortedArrays {
+
+	public static void main(String[] args) {
+		
+	}
+		
+}
