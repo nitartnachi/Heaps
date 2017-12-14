c68a39143cce44b75449859c381cce6609134d67
Nitin Sharma <nsharma78@gmail.com>
2017-12-14 10:39:34 -0800

    IsArrayMinHeap implemented

---
+++ src/main/java/com/nitin/algo/FirstKMaxOccurringWordsInWordsList.java
@@ -0,0 +1,8 @@
+/*
+ * Given a list of strings with duplicate words, find first k max occurring words
+ */
+package com.nitin.algo;
+
+public class FirstKMaxOccurringWordsInWordsList {
+
+}
---
+++ src/main/java/com/nitin/algo/FirstKNonRepeatingInStringOnePass.java
@@ -0,0 +1,9 @@
+/*
+ * For a giving string of alphabets, find first k non-repeating characters in single pass of string.
+ * Example given str = ABCDBAGHCHFAC and k = 3, the output should be D G F
+ */
+package com.nitin.algo;
+
+public class FirstKNonRepeatingInStringOnePass {
+
+}
